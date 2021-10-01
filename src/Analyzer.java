fbdf33dcb8650e94cc4aee476ae4280d27226650
Chris Brooks <chris.cbrooks@gmail.com>
2021-09-30 20:37:40 -0700

    Initial commit

---
+++ src/Analyzer.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Analyzer {
+    protected FreqDist fd;
+
+    public Analyzer() {
+        this.fd = new FreqDist();
+    }
+
+    public void train(List<Review> testReviews) {
+
+    }
+
+
+    public void test(List<Review> testReviews) {
+
+    }
+
+    public void learn() {
+
+    }
+
+}
